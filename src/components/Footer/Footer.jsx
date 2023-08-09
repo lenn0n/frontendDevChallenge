f3b0b99a3f095fa4d94767e45af020045999f9ae
Lennon <gameoveralisa@gmail.com>
2023-08-09 17:01:42 +0800

    Fixed responsiveness

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Footer = () => {
 	return (
 		<div className="footer">
-      created by <span className="fw-bold ms-2 me-2">lenn0n</span> - devChallenges.io
+    created by <span className="fw-bold ms-2 me-2">lenn0n</span> - devChallenges.io
     </div>
 	)
 }
