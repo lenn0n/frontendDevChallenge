32bebbe76421ebefad5230331ee29c4f18ac4477
xennon <gameoveralisa@gmail.com>
2023-08-14 22:32:48 +0800

    Updated files

--- src/pages/ResponsiveWebDev/FourthStage/Recipe.jsx
+++ src/pages/ResponsiveWebDev/FourthStage/Recipe.jsx
@@ -18,7 +18,38 @@ const Recipe = () => {
           <div className="rp-header-cover-container">
             <img src={CheeseCake} alt="CheeseCake" className="rp-header-cover"/>
           </div>
-         
+          <div className="d-flex">
+            <div className="ing-items">
+              <div className="rp-ingredient-title">
+                Ingredients
+              </div>
+              <div className="rp-ingredient-item">
+                Graham Cracker Crust
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="1"/>
+                <label class="form-check-label rp-ingredient-sub-item" for="1">
+                1 and 1/2 cups (150g) <strong>graham cracker crumbs</strong> (about 10 full sheet graham crackers)
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="2"/>
+                <label class="form-check-label rp-ingredient-sub-item" for="2">
+                5 Tablespoons (70g) <strong>unsalted butter</strong>, melted
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="3"/>
+                <label class="form-check-label rp-ingredient-sub-item" for="3">
+                1/4 cup (50g) <strong>granulated sugar</strong>
+                </label>
+              </div>
+            </div>
+            <div className="ing-summary">
+              [ NEXT ITEMS ]
+            </div>
+          </div>
+          
         </div>
       </div>
     </>
