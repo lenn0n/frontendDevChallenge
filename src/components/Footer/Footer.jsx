004cc9cc561ac405c1db9111a18524e7d9b5a7c4
xennon <gameoveralisa@gmail.com>
2023-08-16 21:57:17 +0800

    Updated gallery

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,10 @@ const Footer = () => {
   const navigate = useNavigate()
 	return (
 		<>
-      <div className="footer">
-      created by <span className="fw-bold ms-2 me-2">lenn0n</span> - devChallenges.io
+      <div className="footer d-flex flex-wrap">
+      <div>created by </div>
+      <span className="fw-bold ms-2 me-2">lenn0n</span> 
+      <div>- devChallenges.io</div>
       </div>
       <div className="text-center mb-4 text-dark fw-bold" onClick={()=>{ navigate("/")}} role="button">BACK TO HOMEPAGE</div>
     </>
