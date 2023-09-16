e4c85a4995739d9a5f2be3ee8e1deb18170dc64b
xennon <gameoveralisa@gmail.com>
2023-09-17 00:07:36 +0800

    Fixed grid styles

--- src/pages/ResponsiveWebDev/FirstStage/NotFound.jsx
+++ src/pages/ResponsiveWebDev/FirstStage/NotFound.jsx
@@ -33,32 +33,6 @@ const NotFound = () => {
 					</div>
 				</div>
 			</div>
-			{/* <div className="container mx-auto grid">
-				<div className="grid place-items-center lg:grid-cols-2">
-					<div className="ms-4 me-4 w-[1/4] md:w-9/12 md:ms-0 me-0">
-						<img src={Scarecrow} />
-					</div>
-					<div>
-						<div className="title">
-							I have bad{" "}
-							<span className="hover:text-rose-500">news</span> for
-							you
-						</div>
-						<div className="desc mt-4 mb-4 ms-2 me-2 md:ms-0 me-0">
-							The page you are looking for might be removed or is
-							temporarily unavailable
-						</div>
-						<div className="grid place-content-center md:place-content-start">
-							<button
-								onClick={handleBackToHome}
-								className="px-[43px] py-[24px] uppercase font-bold text-white bg-gray-900 mt-4 mb-4 hover:bg-slate-900 hover:rounded-lg"
-							>
-								Back To Homepage
-							</button>
-						</div>
-					</div>
-				</div>
-			</div> */}
 		</>
 	);
 };
--- src/pages/ResponsiveWebDev/SecondStage/TeamPage.jsx
+++ src/pages/ResponsiveWebDev/SecondStage/TeamPage.jsx
@@ -42,7 +42,7 @@ const TeamPage = () => {
 
 	const Crew = ({ data }) => {
 		return (
-			<div className="flex justify-even sm:basis-1/2 md:basis-1/3 crew-frame">
+			<div className="crew-frame">
 				<div className="flex justify-center">
 					<div>
 						<img
@@ -62,7 +62,7 @@ const TeamPage = () => {
 		<>
 			<div className="container mx-auto flex flex-wrap items-center justify-center">
 				<div style={{ width: "1000px" }}>
-					<div className="flex flex-wrap items-center justify-center">
+					<div className="flex flex-wrap items-center justify-center mb-5">
 						<div className="flex flex-wrap">
 							<div className="basis-full m-4 md:basis-1/2 md:m-0 team-title">
 								<div className="">The creative crew</div>
@@ -77,7 +77,7 @@ const TeamPage = () => {
 							</div>
 						</div>
 					</div>
-					<div className="flex flex-wrap items-center justify-center">
+					<div className="grid sm:grid-cols-2 md:grid-cols-3">
 						{CrewList.map((data) => (
 							<Crew data={data} />
 						))}
