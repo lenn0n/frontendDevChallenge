9d36d5488c19e31b605a2c3f43a918f646c92a2c
Lennon <gameoveralisa@gmail.com>
2023-09-18 16:57:33 +0800

    Updated interior design page

--- src/pages/ResponsiveWebDev/ThirdStage/InteriorConsultant.jsx
+++ src/pages/ResponsiveWebDev/ThirdStage/InteriorConsultant.jsx
@@ -39,84 +39,92 @@ const InteriorConsultant = () => {
 					</div>
 				</div>
 			</div>
-			<div className="interior-consultant-container">
-				<div className="flex justify-between items-center">
+			<div className="bg-[#181719] text-white">
+				<div className="container mx-auto flex flex-wrap justify-center items-center">
 					<div>
-						<div className="ic-header-logo">THIS INTERIOR</div>
-					</div>
-					<div className="ic-header-menu gap-5">
-						<div
-							className="ic-header-item ic-header-menu-selected"
-							role="button"
-						>
-							Home
-						</div>
-						<div className="ic-header-item" role="button">
-							Collection
-						</div>
-						<div className="ic-header-item" role="button">
-							About
-						</div>
-						<div className="ic-header-item" role="button">
-							Contact
-						</div>
-					</div>
-					<div
-						className="hamburger"
-						role="button"
-						onClick={() => {
-							setOpenNav(!openNav);
-						}}
-					>
-						<img src={Burger} width={50} height={50} alt="" />
-					</div>
-				</div>
-
-				<div className="mt-5 flex flex-wrap items-center justify-center">
-					<div className="basis-full md:basis-1/2">
-						<div className="col-9">
-							<div className="ic-body-title">Modern interior</div>
-							<div className="ic-body-desc">
-								A full-Service residential & commercial interior
-								design and staging company offering professional
-								organizing & eco-services.
+						<div className="flex justify-between items-center mb-5">
+							<div>
+								<div className="ic-header-logo">THIS INTERIOR</div>
+							</div>
+							<div className="ic-header-menu gap-5">
+								<div
+									className="ic-header-item ic-header-menu-selected"
+									role="button"
+								>
+									Home
+								</div>
+								<div className="ic-header-item" role="button">
+									Collection
+								</div>
+								<div className="ic-header-item" role="button">
+									About
+								</div>
+								<div className="ic-header-item" role="button">
+									Contact
+								</div>
 							</div>
 							<div
-								className="mt-5 fw-bold ic-header-text mb-4 flex gap-2"
+								className="hamburger"
 								role="button"
+								onClick={() => {
+									setOpenNav(!openNav);
+								}}
 							>
-								Read more{" "}
-								<span className="material-icons">
-									arrow_right_alt
-								</span>
+								<img src={Burger} width={50} height={50} alt="" />
 							</div>
 						</div>
-					</div>
 
-					<div className="basis-full md:basis-1/2">
-						<img src={ICCover} alt="Loading" className="ic-cover" />
-						<div className="flex justify-end">
-							<div className="ic-author-container">
-								<div className="flex items-center m-4">
-									<img
-										src={ICAvatar}
-										className="ic-avatar"
-										alt="author"
-										height={50}
-										width={50}
-									/>
-									<div className="ms-3">
-										<div className="ic-author">Aliza Webber</div>
-										<div className="ic-position  hover:text-white">
-											Interior designer
-										</div>
+						<div className="mt-5 flex flex-wrap items-center justify-center">
+							<div className="basis-full md:basis-1/2">
+								<div className="col-9">
+									<div className="ic-body-title">Modern interior</div>
+									<div className="ic-body-desc">
+										A full-Service residential & commercial interior
+										design and staging company offering professional
+										organizing & eco-services.
+									</div>
+									<div
+										className="mt-5 fw-bold ic-header-text mb-4 flex gap-2"
+										role="button"
+									>
+										Read more{" "}
+										<span className="material-icons">
+											arrow_right_alt
+										</span>
 									</div>
 								</div>
-								<div
-									className="ic-banner ps-4 pe-4 pt-1"
-									style={{ width: "287px" }}
-								>
-									Designed in 2020 by Aliza Webber
+							</div>
+
+							<div className="basis-full md:basis-1/2">
+								<img
+									src={ICCover}
+									alt="Loading"
+									className="ic-cover"
+								/>
+								<div className="flex justify-end">
+									<div className="ic-author-container">
+										<div className="flex items-center m-4">
+											<img
+												src={ICAvatar}
+												className="ic-avatar"
+												alt="author"
+												height={50}
+												width={50}
+											/>
+											<div className="ms-3">
+												<div className="ic-author">Aliza Webber</div>
+												<div className="ic-position  hover:text-white">
+													Interior designer
+												</div>
+											</div>
+										</div>
+										<div
+											className="ic-banner ps-4 pe-4 pt-1"
+											style={{ width: "287px" }}
+										>
+											Designed in 2020 by Aliza Webber
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
