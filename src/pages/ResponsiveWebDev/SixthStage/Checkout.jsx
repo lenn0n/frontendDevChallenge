9f6ea118c4e24cc094d3b093a2f6c1c06b177fb4
xennon <gameoveralisa@gmail.com>
2023-09-19 23:54:43 +0800

    Updated checkout page

--- src/pages/ResponsiveWebDev/SixthStage/Checkout.jsx
+++ src/pages/ResponsiveWebDev/SixthStage/Checkout.jsx
@@ -10,14 +10,14 @@ const Checkout = () => {
 		return (
 			<div className="checkout-form w-full">
 				<div className="checkout-label">{title}</div>
-				<div className="flex flex-column">
+				<div className="flex flex-col">
 					<input
 						type="text"
 						className="checkout-input block rounded-lg w-full focus:border-gray-500 outline-none border-2 focus:bg-gray-100 peer/input"
 						placeholder={placeholder}
-						autoComplete="off"
+						autocomplete="off"
 					/>
-					<div className="material-icon input-field-icon peer-focus/input:rotate-45 transition ">
+					<div className="material-icons input-field-icon peer-focus/input:rotate-45 transition ">
 						{icon}
 					</div>
 					<div className="text-custom-primary font-medium hidden peer-focus/input:block peer-focus/input:transition before:content-['*'] before:text-red-500 before:mr-1">
@@ -36,7 +36,7 @@ const Checkout = () => {
 		return (
 			<div className="checkout-form w-full">
 				<div className="checkout-label">{title}</div>
-				<div className="flex flex-column">
+				<div className="flex flex-col">
 					<select
 						className="checkout-select rounded-lg w-full focus:border-gray-500 outline-none border-2 focus:bg-gray-100 peer/select"
 						defaultValue=""
@@ -49,10 +49,10 @@ const Checkout = () => {
 						<option value="ph">Philippines</option>
 						<option value="us">United States</option>
 					</select>
-					<span className="material-icons input-field-icon peer-focus/select:rotate-45 transition  animate-spin">
+					<span className="material-icons input-field-icon peer-focus/select:rotate-45 transition animate-spin">
 						{icon}
 					</span>
-					<span className="material-icons input-field-bgicon  rounded-md">
+					<span className="material-icons input-field-bgicon rounded-md">
 						expand_more
 					</span>
 				</div>
@@ -60,83 +60,90 @@ const Checkout = () => {
 		);
 	};
 	return (
-		<div className="grid place-content-center">
-			<div className="place-items-start checkout-header">
-				Checkout
-			</div>
-			<div className="grid md:grid-cols-2 gap-4">
-				<div className="">
-					<div className="checkout-title">Contact information</div>
-					<InputForm
-						title="Email"
-						icon="mail"
-						placeholder="Enter your email..."
-					/>
-					<InputForm
-						title="Phone"
-						icon="phone"
-						placeholder="Enter your phone..."
-					/>
-					<div className="checkout-title mt-5">
-						Shipping information
-					</div>
-					<InputForm
-						title="Full name"
-						icon="account_circle"
-						placeholder="Your name.."
-					/>
-					<InputForm
-						title="Address"
-						icon="home"
-						placeholder="Your address.."
-					/>
-					<InputForm
-						title="City"
-						icon="location_city"
-						placeholder="Your city.."
-					/>
-					<div className="md:flex justify-between gap-3">
-						<SelectForm
-							title="Country"
-							icon="public"
-							placeholder="Your country.."
-						/>
-						<InputForm
-							title="Postal code"
-							icon="markunread_mailbox"
-							placeholder="Your postal code.."
-						/>
-					</div>
-					<div className="flex gap-2">
-						<input type="checkbox" name="save" />
-						<label for="save" className="fw-bold checkout-label ">
-							Save this information for next time
-						</label>
-					</div>
-					<div className="grid place-items-end">
-						<button className="checkout-button text-white mt-4">
-							Continue
-						</button>
-					</div>
+		<>
+			<div className="container mx-auto flex flex-col">
+				<div className="place-items-start checkout-header ms-4 me-4">
+					Checkout
 				</div>
-
-				<div className="ms-4">
-					<div className="checkout-panel-card rounded-xl m-3 p-4">
-						<div className="flex gap-2">
-							<img
-								className="rounded-3"
-								src={Item1}
-								height={134}
-								width={134}
+				<div className="grid md:grid-cols-2 gap-4">
+					<div className="order-2 md:order-1">
+						<div className="ms-4 me-4">
+							<div className="checkout-title">
+								Contact information
+							</div>
+							<InputForm
+								title="Email"
+								icon="mail"
+								placeholder="Enter your email..."
 							/>
-							<div className="text-[16px] font-[600]">
-								Vintage Backbag
+							<InputForm
+								title="Phone"
+								icon="phone"
+								placeholder="Enter your phone..."
+							/>
+							<div className="checkout-title mt-5">
+								Shipping information
+							</div>
+							<InputForm
+								title="Full name"
+								icon="account_circle"
+								placeholder="Your name.."
+							/>
+							<InputForm
+								title="Address"
+								icon="home"
+								placeholder="Your address.."
+							/>
+							<InputForm
+								title="City"
+								icon="location_city"
+								placeholder="Your city.."
+							/>
+							<div className="md:flex justify-between gap-3">
+								<SelectForm
+									title="Country"
+									icon="public"
+									placeholder="Your country.."
+								/>
+								<InputForm
+									title="Postal code"
+									icon="markunread_mailbox"
+									placeholder="Your postal code.."
+								/>
+							</div>
+							<div className="flex gap-2">
+								<input type="checkbox" name="save" />
+								<label for="save" className="fw-bold checkout-label ">
+									Save this information for next time
+								</label>
+							</div>
+							<div className="grid place-items-end">
+								<button className="checkout-button text-white mt-4">
+									Continue
+								</button>
+							</div>
+						</div>
+					</div>
+					<div className="order-1 md:order-2 ">
+						<div>
+							<div className="checkout-panel-card rounded-xl m-3 p-4">
+								<div className="flex gap-2">
+									<img
+										className="rounded-3"
+										src={Item1}
+										height={134}
+										width={134}
+									/>
+									<div className="text-[16px] font-[600]">
+										Vintage Backbag
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
