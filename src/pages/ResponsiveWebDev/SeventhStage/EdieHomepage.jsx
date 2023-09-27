f1dade2a3b0c659af868310ec50a90157ed39fd4
xennon <gameoveralisa@gmail.com>
2023-09-27 23:09:18 +0800

    updated edie page

--- src/pages/ResponsiveWebDev/SeventhStage/EdieHomepage.jsx
+++ src/pages/ResponsiveWebDev/SeventhStage/EdieHomepage.jsx
@@ -1,8 +1,136 @@
 import React from "react"
 import Footer from "@components/Footer/Footer"
-
+import HeroImage from "@assets/images/edie/heroImage.jpg"
 const EdieHomepage = () => {
   return (<>
+  <div>
+    <div className="">Eddie</div>
+    <div className="">
+      <div>Home</div>
+      <div>Services</div>
+      <div>Our Works</div>
+      <div>Clients</div>
+      <div>Contact</div>
+    </div>
+  </div>
+  <div>
+    <div>
+      Unhappy with your website?
+    </div>
+    <div>
+      Unhappy with your website?
+    </div>
+  </div>
+  <div>
+    <img src={HeroImage} alt="..."/>
+  </div>
+  <div className="">
+    <div className="">Story, emotion and purpose</div>
+    <div className="">We help transform your ideas into real world applications 
+    that will outperform your toughest competition and help you achieve your strategic goals in short period of time.</div>
+    <div className="">
+      <div className="">Want us to contact you?</div>
+      <div>
+        <input type="email"/>
+        <div className="button">Submit</div>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    We offer high demand services
+  </div>
+
+  <div className="">
+    <div className="">
+      <div>ICON</div>
+      <div className="">UI/UX Design</div>
+      <div className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac nulla consequat aliquet id quis turpis.</div>
+      <div className="">
+        <div className="button">Get Started</div>
+      </div>
+    </div>
+    <div className="">
+      <div>ICON</div>
+      <div className="">Front End</div>
+      <div className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac nulla consequat aliquet id quis turpis.</div>
+      <div className="">
+        <div className="button">Get Started</div>
+      </div>
+    </div>
+    <div className="">
+      <div>ICON</div>
+      <div className="">Backend</div>
+      <div className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a metus ac nulla consequat aliquet id quis turpis.</div>
+      <div className="">
+        <div className="button">Get Started</div>
+      </div>
+    </div>
+    
+  </div>
+
+  <div className="">
+    Good design means good business
+  </div>
+
+  <div className="">
+    <div className="">
+      <div className="">IMG</div>
+      <div className="">Full stack application</div>
+      <div className="">Smart home dashboard</div>
+    </div>
+    <div className="">
+      <div className="">IMG</div>
+      <div className="">UI/UX Design</div>
+      <div className="">Onboard application</div>
+    </div>
+    <div className="">
+      <div className="">IMG</div>
+      <div className="">Mobile application</div>
+      <div className="">Booking System</div>
+    </div>
+    <div className="">
+      <div className="">IMG</div>
+      <div className="">Front End application</div>
+      <div className="">Juice product homepage</div>
+    </div>
+  </div>
+
+  <div>See more ---</div>
+
+  <div className="">
+    <div className="">
+      <div className="">Meet the team</div>
+      <div className="">We are chilled and a laidback team</div>
+      <div className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+    </div>
+
+    <div className="">
+      <div className="">
+        <div>
+          IMG 1
+        </div>
+        <div className="">
+          <div className="">IMG2</div>
+          <div className="">IMG3</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="">“Fast and outstanding resutls. Edie understands their customer’s needs. They have a young and talented team.” </div>
+  
+  <div className="">
+    <div className="">DP</div>
+    <div className="">
+      <div className="">Carlos Tran</div>
+      <div className="">The Decorate Gatsby</div>
+    </div>
+  </div>
+
+    <div className="">Footer</div>
+  
   <Footer/>
   </>)
 }
